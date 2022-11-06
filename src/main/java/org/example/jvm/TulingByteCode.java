042b2bfaa45fedb17c0e7c28d9b823d8d3d05dbd
tian <1447660461@qq.com>
2022-11-06 23:59:46 +0800

    jvm

---
+++ src/main/java/org/example/jvm/TulingByteCode.java
@@ -0,0 +1,15 @@
+package org.example.jvm;
+
+public class TulingByteCode {
+
+    private String userName;
+
+    public String getUserName() {
+
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
